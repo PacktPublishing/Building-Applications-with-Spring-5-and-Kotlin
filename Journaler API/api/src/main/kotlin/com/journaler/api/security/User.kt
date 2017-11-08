d0498f1df534b42b947a187901bbc7f8dc444c35
Milos Vasic <milos85vasic@gmail.com>
2017-11-08 16:42:35 +0100

    User class.

---
+++ Journaler API/api/src/main/kotlin/com/journaler/api/security/User.kt
@@ -0,0 +1,80 @@
+package com.journaler.api.security
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.UpdateTimestamp
+import org.hibernate.validator.constraints.Email
+import org.hibernate.validator.constraints.NotBlank
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.*
+import javax.persistence.*
+import javax.validation.constraints.NotNull
+
+@Entity
+@Table(name = "user")
+data class User(
+        @Id
+        @GeneratedValue(generator = "uuid2")
+        @GenericGenerator(name = "uuid2", strategy = "uuid2")
+        @Column(columnDefinition = "varchar(36)")
+        var id: String = "",
+
+        @Column(unique = true, nullable = false)
+        @NotNull
+        @Email
+        var email: String,
+
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+        @NotBlank
+        var pwd: String,
+
+        @NotBlank
+        var firstName: String,
+
+        @NotBlank
+        var lastName: String,
+
+        var roles: String,
+        var enabled: Boolean,
+        var accountNonExpired: Boolean,
+        var accountNonLocked: Boolean,
+        var credentialsNonExpired: Boolean,
+
+        @CreationTimestamp
+        var created: Date,
+
+        @UpdateTimestamp
+        var modified: Date
+
+) : UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        val authorities = mutableListOf<GrantedAuthority>()
+        roles
+                .split(",")
+                .forEach { it ->
+                    authorities.add(
+                            SimpleGrantedAuthority(
+                                    it.trim()
+                            )
+                    )
+                }
+        return authorities
+    }
+
+    override fun isEnabled() = enabled
+
+    override fun getUsername() = email
+
+    override fun isCredentialsNonExpired() = credentialsNonExpired
+
+    override fun getPassword() = pwd
+
+    override fun isAccountNonExpired() = accountNonExpired
+
+    override fun isAccountNonLocked() = accountNonLocked
+
+}
