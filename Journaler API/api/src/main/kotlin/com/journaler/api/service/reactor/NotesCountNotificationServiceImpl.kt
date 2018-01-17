8046d7df3dac45e4fab51f1122c58a634f82ff68
Milos Vasic <milos85vasic@gmail.com>
2018-01-17 16:08:31 +0100

    Started work on React services implementation.

---
+++ Journaler API/api/src/main/kotlin/com/journaler/api/service/reactor/NotesCountNotificationServiceImpl.kt
@@ -0,0 +1,13 @@
+package com.journaler.api.service.reactor
+
+import com.journaler.api.reactor.NotesCountNotification
+import org.springframework.stereotype.Service
+
+@Service
+class NotesCountNotificationServiceImpl : NotesCountNotificationService {
+
+    override fun notify(notification: NotesCountNotification) {
+
+    }
+
+}
