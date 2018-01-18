e418481b6d032b2030aa3e934f584b07fbda0b84
Milos Vasic <milos85vasic@gmail.com>
2018-01-18 17:47:12 +0100

    Reactor notification services implementation completed.

--- Journaler API/api/src/main/kotlin/com/journaler/api/service/reactor/TodosCountNotificationServiceImpl.kt
+++ Journaler API/api/src/main/kotlin/com/journaler/api/service/reactor/TodosCountNotificationServiceImpl.kt
@@ -2,20 +2,18 @@ package com.journaler.api.service.reactor
 
 import com.journaler.api.mail.MailMessage
 import com.journaler.api.mail.MailService
-import com.journaler.api.reactor.NotesCountNotification
+import com.journaler.api.reactor.TodosCountNotification
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
 
-@Service
-class NotesCountNotificationServiceImpl : NotesCountNotificationService {
+class TodosCountNotificationServiceImpl : TodosCountNotificationService {
 
     @Autowired
     private lateinit var mailService: MailService
 
-    override fun notify(notification: NotesCountNotification) {
+    override fun notify(notification: TodosCountNotification) {
         val to = "your_email@example.com"
         val subject = "Notes count notification"
-        val text = "Notes reached ${notification.notesCount} count."
+        val text = "Todos reached ${notification.todosCount} count."
         val message = MailMessage(to, subject, text)
         mailService.sendMessage(message)
     }
