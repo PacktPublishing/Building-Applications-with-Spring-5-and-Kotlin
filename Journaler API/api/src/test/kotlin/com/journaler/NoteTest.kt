b9cfb2ff19e0a702ac40e3e75d58bd597b02ea6d
Milos Vasic <milos85vasic@gmail.com>
2018-02-16 13:34:03 +0100

    Execute in order.

--- Journaler API/api/src/test/kotlin/com/journaler/NoteTest.kt
+++ Journaler API/api/src/test/kotlin/com/journaler/NoteTest.kt
@@ -12,32 +12,37 @@ class NoteTest {
     }
 
     @Test
-    fun insert() {
-        println("Insert.")
-        // Test insert operation for Note entity.
-    }
-
-    @Test
-    fun update() {
-        println("Update.")
-        // Test update operation for Note entity.
-    }
-
-    @Test
-    fun delete() {
-        println("Delete.")
-        // Test delete operation for Note entity.
-    }
-
-    @Test
-    fun select() {
-        println("Select.")
-        // Test select operation for Note entity.
+    fun crud() {
+        // Test Note entity CRUD operations.
+        insert()
+        update()
+        delete()
+        select()
     }
 
     @After
     fun cleanup() {
         println("Cleanup.")
         // Do cleanup after all tests are performed.
+    }
+
+    fun insert() {
+        println("Insert.")
+        // Test insert operation for Note entity.
+    }
+
+    fun update() {
+        println("Update.")
+        // Test update operation for Note entity.
+    }
+
+    fun delete() {
+        println("Delete.")
+        // Test delete operation for Note entity.
+    }
+
+    fun select() {
+        println("Select.")
+        // Test select operation for Note entity.
     }
 }
