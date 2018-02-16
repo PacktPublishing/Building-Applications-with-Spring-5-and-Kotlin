f9d66c1f0b6311c129e4729a77e4834c008c5dc4
Milos Vasic <milos85vasic@gmail.com>
2018-02-16 13:15:37 +0100

    Before and after.

--- Journaler API/api/src/test/kotlin/com/journaler/NoteTest.kt
+++ Journaler API/api/src/test/kotlin/com/journaler/NoteTest.kt
@@ -1,21 +1,37 @@
 package com.journaler
 
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class NoteTest {
+    @Before
+    fun prepare(){
+        // Prepare environment and requirements for tests to be performed.
+    }
+
     @Test
     fun insert(){
+        // Test insert operation for Note entity.
     }
 
     @Test
     fun update(){
+        // Test update operation for Note entity.
     }
 
     @Test
     fun delete(){
+        // Test delete operation for Note entity.
     }
 
     @Test
     fun select(){
+        // Test select operation for Note entity.
+    }
+
+    @After
+    fun cleanup(){
+        // Do cleanup after all tests are performed.
     }
 }
