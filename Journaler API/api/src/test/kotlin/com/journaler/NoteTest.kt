e646d75c87d863d58bc73c3bcf92d226f338c42d
Milos Vasic <milos85vasic@gmail.com>
2018-02-25 13:04:46 +0100

    Todo entity test.

--- Journaler API/api/src/test/kotlin/com/journaler/NoteTest.kt
+++ Journaler API/api/src/test/kotlin/com/journaler/NoteTest.kt
@@ -3,7 +3,6 @@ package com.journaler
 import com.journaler.api.ApiApplication
 import com.journaler.api.data.NoteDTO
 import com.journaler.api.service.NoteService
-import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
